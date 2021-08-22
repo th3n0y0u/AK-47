485544907446d95fa10c7bfd846d56b2d7898537
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-08-22 16:33:51 -0700

    Update AnimationScript.lua

--- AnimationScript.lua
+++ AnimationScript.lua
@@ -1,34 +1,60 @@
-game.Players.PlayerAdded:Connect(function(player)			
-	player.CharacterAdded:Connect(function(char)	
+local function mainclass()
+	local function playeraddedtoserver(player)		
+		if player then
+			player.CharacterAdded:Connect(function(char)	
+				if char then
+					local success, errormessage = pcall(function()
+						local toolgrip = Instance.new("Motor6D", char.UpperTorso) 
+						toolgrip.Name = "ToolGrip" 
+					end)
+					
+					if not success then
+						warn(errormessage)
+						char:BreakJoints()
+					end 
+				end
+			end) 
+		end
+	end
+
+	local function connectM6D(player, location, tool)
 		local success, errormessage = pcall(function()
-			local toolgrip = Instance.new("Motor6D", char.UpperTorso) 
-			toolgrip.Name = "ToolGrip" 
+			if player and location and tool then
+				local realtool = player.Backpack:FindFirstChild(tool.Name) or player.Character:FindFirstChild(tool.Name)
+				if location == realtool:FindFirstChild("BodyAttach") then
+					local char = player.Character
+					char.UpperTorso.ToolGrip.Part0 = char.UpperTorso
+					char.UpperTorso.ToolGrip.Part1 = location
+				end
+			end
 		end)
 		
 		if not success then
 			warn(errormessage)
-		end 
-	end) 
-end)
-
-game.ReplicatedStorage.ConnectM6D.OnServerEvent:Connect(function(player, location)
-	local success, errormessage = pcall(function()
-		local char = player.Character
-		char.UpperTorso.ToolGrip.Part0 = char.UpperTorso
-		char.UpperTorso.ToolGrip.Part1 = location
-	end)
-	
-	if not success then
-		warn(errormessage)
+			player.Character:BreakJoints() 
+		end
 	end
-end) 
 
-game.ReplicatedStorage.DisconnectM6D.OnServerEvent:Connect(function(player)
-	local success, errormessage = pcall(function()
-		player.Character.UpperTorso.ToolGrip.Part1 = nil
-	end)
-	
-	if not success then
-		warn(errormessage)
+	local function disconnectM6D(player, tool)
+		local success, errormessage = pcall(function()
+			if player and tool then
+				local realtool = player.Backpack:FindFirstChild(tool.Name) or player.Character:FindFirstChild(tool.Name)
+				if realtool then
+					player.Character.UpperTorso.ToolGrip.Part1 = nil
+				end
+			end
+		end)
+		
+		if not success then
+			warn(errormessage)
+			player.Character:BreakJoints() 
+		end
 	end
-end) 
+
+	game.Players.PlayerAdded:Connect(playeraddedtoserver)
+	game.ReplicatedStorage.ConnectM6D.OnServerEvent:Connect(connectM6D)
+	game.ReplicatedStorage.DisconnectM6D.OnServerEvent:Connect(disconnectM6D)
+end
+
+mainclass()
+ 
