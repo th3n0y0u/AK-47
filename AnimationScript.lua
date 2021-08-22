a1ab88e36696dc67fad62995d68ca5b2cf3c62a5
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-08-22 16:20:47 -0700

    Update AnimationScript.lua

--- AnimationScript.lua
+++ AnimationScript.lua
@@ -1,16 +1,34 @@
 game.Players.PlayerAdded:Connect(function(player)			
-	player.CharacterAdded:Connect(function(char)		
-		local toolgrip = Instance.new("Motor6D", char.UpperTorso)
-		toolgrip.Name = "ToolGrip"
-	end)
+	player.CharacterAdded:Connect(function(char)	
+		local success, errormessage = pcall(function()
+			local toolgrip = Instance.new("Motor6D", char.UpperTorso) 
+			toolgrip.Name = "ToolGrip" 
+		end)
+		
+		if not success then
+			warn(errormessage)
+		end 
+	end) 
 end)
 
 game.ReplicatedStorage.ConnectM6D.OnServerEvent:Connect(function(player, location)
-	local char = player.Character
-	char.UpperTorso.ToolGrip.Part0 = char.UpperTorso
-	char.UpperTorso.ToolGrip.Part1 = location
+	local success, errormessage = pcall(function()
+		local char = player.Character
+		char.UpperTorso.ToolGrip.Part0 = char.UpperTorso
+		char.UpperTorso.ToolGrip.Part1 = location
+	end)
+	
+	if not success then
+		warn(errormessage)
+	end
 end) 
 
 game.ReplicatedStorage.DisconnectM6D.OnServerEvent:Connect(function(player)
-	player.Character.UpperTorso.ToolGrip.Part1 = nil
+	local success, errormessage = pcall(function()
+		player.Character.UpperTorso.ToolGrip.Part1 = nil
+	end)
+	
+	if not success then
+		warn(errormessage)
+	end
 end) 
